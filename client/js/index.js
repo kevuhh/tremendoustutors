9d4fce1adc502334a344ba430356b27d83c83451
kevuhh <kaboo8x@gmail.com>
2021-02-16 12:00:37 -0500

    change layout and remove dim

--- client/js/index.js
+++ client/js/index.js
@@ -3,7 +3,8 @@ $(document).ready(
         initializeAttributes()
         $('.ui.sidebar')
             .sidebar({
-                context: $('.bottom.segment')
+                context: $('.bottom.segment'),
+                dimPage: false
             });
         $('.menu .item').on('click', () => {
             $('.ui.sidebar')
@@ -14,32 +15,30 @@ $(document).ready(
             $('.pusher .segment.home').removeClass('hide');
             $('.pusher .segment').addClass('hide');
             $('.pusher .segment.home').removeClass('hide');
-        });
-        $('.contact.item').on('click', () => {
-            $('.pusher .segment.contact').removeClass('hide');
-            $('.pusher .segment').addClass('hide');
-            $('.pusher .segment.contact').removeClass('hide');
+            $('.pusher .segment.footer').removeClass('hide');
         });
         $('.faq.item').on('click', () => {
             $('.pusher .segment.faq').removeClass('hide');
             $('.pusher .segment').addClass('hide');
             $('.pusher .segment.faq').removeClass('hide');
+            $('.pusher .segment.footer').removeClass('hide');
         });
         $('.schedule.item').on('click', () => {
             $('.pusher .segment.schedule').removeClass('hide');
             $('.pusher .segment').addClass('hide');
             $('.pusher .segment.schedule').removeClass('hide');
+            $('.pusher .segment.footer').removeClass('hide');
         });
         $('.meet.item').on('click', () => {
             $('.pusher .segment.meet').removeClass('hide');
             $('.pusher .segment').addClass('hide');
             $('.pusher .segment.meet').removeClass('hide');
+            $('.pusher .segment.footer').removeClass('hide');
         });
     }
 );
 
 function initializeAttributes() {
-    $('.pusher .segment.contact').addClass('hide');
     $('.pusher .segment.faq').addClass('hide');
     $('.pusher .segment.schedule').addClass('hide');
     $('.pusher .segment.meet').addClass('hide');
